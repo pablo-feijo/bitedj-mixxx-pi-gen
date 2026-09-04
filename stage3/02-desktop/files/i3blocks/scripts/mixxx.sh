ad8d70ca4a44c4feb8e76b55933e1b1b3211ffc2
Pablo Feijo <devpablofeijo@gmail.com>
2026-09-04 18:51:00 -0300

    fix:  Waybar/i3blocks launch script to use correct environment variables

--- stage3/02-desktop/files/i3blocks/scripts/mixxx.sh
+++ stage3/02-desktop/files/i3blocks/scripts/mixxx.sh
@@ -3,8 +3,8 @@
 if ! pgrep -x "bitedj" > /dev/null && ! pgrep -x "mixxx" > /dev/null
 then
     if [ -x "/usr/bin/bitedj" ]; then
-        swaymsg exec "/usr/bin/bitedj"
+        swaymsg exec "env PA_ALSA_PLUGHW=1 WLR_DRM_NO_MODIFIERS=1 QT_WAYLAND_SHELL_INTEGRATION=xdg-shell /usr/bin/bitedj --resourcePath /usr/share/mixxx/ --full-screen --style Fusion"
     else
-        swaymsg exec "/usr/bin/mixxx"
+        swaymsg exec "env PA_ALSA_PLUGHW=1 WLR_DRM_NO_MODIFIERS=1 QT_WAYLAND_SHELL_INTEGRATION=xdg-shell /usr/bin/mixxx --resourcePath /usr/share/mixxx/ --full-screen --style Fusion"
     fi
 fi
