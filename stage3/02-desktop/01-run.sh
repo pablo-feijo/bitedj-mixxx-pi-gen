1aee9e96d66eef20463c48129ded6e7903b97f24
Pablo Feijo <devpablofeijo@gmail.com>
2026-09-04 16:49:43 -0300

    chore: Allow pi user passwordless udisks2 operations for USB eject

--- stage3/02-desktop/01-run.sh
+++ stage3/02-desktop/01-run.sh
@@ -14,3 +14,6 @@ install -m 440 files/010_pi-nopasswd ${ROOTFS_DIR}/etc/sudoers.d/
 
 echo pi - memlock unlimited >> ${ROOTFS_DIR}/etc/security/limits.conf
 echo pi - rtprio 99 >> ${ROOTFS_DIR}/etc/security/limits.conf
+
+# Allow pi user to mount and unmount USB drives without password
+install -m 644 files/50-udisks.rules ${ROOTFS_DIR}/etc/polkit-1/rules.d/
