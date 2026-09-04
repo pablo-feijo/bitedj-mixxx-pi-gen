3e76ab019183a0ce5f1f698e4895abe969425adf
Pablo Feijo <devpablofeijo@gmail.com>
2026-09-04 10:32:21 -0300

    docs: Update OS config for BiteDJ

--- stage3/03-kernel-setup/00-run.sh
+++ stage3/03-kernel-setup/00-run.sh
@@ -1,4 +1,16 @@
-# Add preempt=full kernel command line argument
+# Add preempt=full and splash kernel command line argument
 on_chroot << EOF
-    sed -i 's/$/ preempt=full cpufreq.default_governor=performance/' /boot/firmware/cmdline.txt
+    sed -i 's/$/ preempt=full cpufreq.default_governor=performance quiet splash plymouth.ignore-serial-consoles logo.nologo vt.global_cursor_default=0/' /boot/firmware/cmdline.txt
 EOF
+
+# Install custom Plymouth theme
+mkdir -p "${ROOTFS_DIR}/usr/share/plymouth/themes/bitedj"
+cp files/bitedj-plymouth/bitedj.plymouth "${ROOTFS_DIR}/usr/share/plymouth/themes/bitedj/"
+cp files/bitedj-plymouth/bitedj.script "${ROOTFS_DIR}/usr/share/plymouth/themes/bitedj/"
+# We copy the new pioneer splash from the plymouth theme folder
+cp files/bitedj-plymouth/pioneer.png "${ROOTFS_DIR}/usr/share/plymouth/themes/bitedj/wallpaper.png"
+
+on_chroot << EOF
+    plymouth-set-default-theme -R bitedj
+EOF
+
