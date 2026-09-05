0b57f29ff8334028ca692801c3f02f52c6f01711
Pablo Feijo <devpablofeijo@gmail.com>
2026-09-05 02:46:10 -0300

    feat(ui): add disconnect button to bluetooth window

--- stage3/02-desktop/files/bitedj-bt.sh
+++ stage3/02-desktop/files/bitedj-bt.sh
@@ -11,7 +11,7 @@ swaymsg "fullscreen disable"
 # Get paired devices
 DEVICES=$(bluetoothctl devices Paired | sed 's/Device //g' | awk '{MAC=$1; $1=""; print MAC "\n" $0}')
 
-SELECTION=$(echo -e "PAIR_NEW\n+ Pair New Device...\n$DEVICES" | zenity --list --title="Bluetooth" --text="Tap your headphones to connect:" --column="MAC" --column="Name" --hide-column=1 --width=600 --height=400)
+SELECTION=$(echo -e "DISCONNECT\n[Disconnect Current Device]\nPAIR_NEW\n[Pair New Device...]\n$DEVICES" | zenity --list --title="Bluetooth" --text="Tap an option below:" --column="MAC" --column="Name" --hide-column=1 --width=600 --height=400)
 
 if [ "$SELECTION" == "PAIR_NEW" ]; then
     blueman-manager &
@@ -20,6 +20,11 @@ if [ "$SELECTION" == "PAIR_NEW" ]; then
     wait $NAG_PID
     pkill -9 -f blueman 2>/dev/null
     killall swaynag 2>/dev/null
+elif [ "$SELECTION" == "DISCONNECT" ]; then
+    zenity --info --text="Disconnecting audio..." --timeout=1 --no-wrap &
+    for mac in $(bluetoothctl info | grep "Device" | awk '{print $2}'); do
+        bluetoothctl disconnect "$mac"
+    done
 elif [ -n "$SELECTION" ]; then
     zenity --info --text="Connecting to audio..." --timeout=2 --no-wrap &
     bluetoothctl disconnect "$SELECTION" 2>/dev/null
